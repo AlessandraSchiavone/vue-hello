10dfa511290e2174b12e447be72e78050cba2d04
AlessandraSchiavone <alessandraschiavone31@gmail.com>
2021-05-17 16:11:56 +0200

    Esercizio completo

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Descrizione:
+// Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+var app = new Vue(
+        {
+        el: "#root",
+        data: {
+            message: "Benvenuto nel mondo di VUE",
+            bckimg: "https://www.luigisabbetti.it/wp-content/uploads/2018/04/guida-vue-js-italiano.png"
+        }     
+    })
+
+
+
